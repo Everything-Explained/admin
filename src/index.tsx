59c59405046022f96547c9afb38eb13a84db6b96
Jaeiya <exanix@gmail.com>
2023-07-12 12:50:49 -0700

    clean(index): split long text into 2 lines

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
         console.log('hello world');
     };
     throw new Error(
-        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
+        'Root element not found. Did you forget to add it to your index.html? ' +
+            'Or maybe the id attribute got misspelled?'
     );
 }
 
