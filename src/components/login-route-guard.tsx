cdd920e16c3819010a15d9639b96510b3d761173
Jaeiya <exanix@gmail.com>
2023-08-29 19:21:15 -0700

    feat: add login route guard
    This allows us to redirect to the login page if a user is not yet
    authenticated.

---
+++ src/components/login-route-guard.tsx
@@ -0,0 +1,14 @@
+import { Outlet, useNavigate } from '@solidjs/router';
+
+export function LoginRouteGuard() {
+    const nav = useNavigate();
+
+    const needsLogin = true;
+
+    if (needsLogin) {
+        nav('/login', { replace: true });
+        return null;
+    }
+
+    return <Outlet />;
+}
