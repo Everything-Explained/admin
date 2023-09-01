d9724a0019f2db601a88d9eb1b8a42187cfbdb3d
Jaeiya <exanix@gmail.com>
2023-09-01 15:50:39 -0700

    feat: add nav card

---
+++ src/components/nav-card.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from '@solidjs/router';
+import './nav-card.css';
+import { Button } from './ui/button';
+
+type NavCardProps = {
+    children: string;
+    title: string;
+    to: string;
+    disabled?: boolean;
+};
+
+export function NavCard({ children, title, disabled, to }: NavCardProps) {
+    const isDisabled = disabled ?? false;
+    const nav = useNavigate();
+
+    const notImplemented = <span class="font-normal text-rose-400">NOT IMPLEMENTED.</span>;
+
+    return (
+        <div class="nav-card inline-block rounded-lg bg-gray-700/20">
+            <h1 class="m-0 text-4xl text-yellow-400">{title}</h1>
+            <p class="py-6 text-left text-xl">
+                {isDisabled ? notImplemented : ''} {children}
+            </p>
+            <Button
+                disabled={isDisabled}
+                color="good"
+                click={() => {
+                    nav(to);
+                }}
+            >
+                Go
+            </Button>
+        </div>
+    );
+}
