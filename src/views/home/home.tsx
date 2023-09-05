8f548b53a4485e5631db51d8e8fa29be9453784b
Jaeiya <exanix@gmail.com>
2023-09-04 18:47:27 -0700

    feat(home): add header and adjust verbiage & padding

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -12,17 +12,20 @@ export function Home() {
   // });
 
   return (
-    <div class="home__container mx-10 mt-[10vh] gap-6">
-      <NavCard to="/login" title="Literature">
-        Allows you to manage literature entires: update, add, or delete them. This also includes
-        publishing or unpublishing them, depending on your access level.
-      </NavCard>
-      <NavCard to="/" disabled={true} title="Logs">
-        Allows you to manage the server logs: download or delete them.
-      </NavCard>
-      <NavCard to="/" disabled={true} title="Logs">
-        Allows you to manage users: add, update, or delete them.
-      </NavCard>
-    </div>
+    <>
+      <header class="my-7 text-center text-4xl">Panel Selection</header>
+      <div class="home__container mx-10 mt-4 gap-6">
+        <NavCard to="/login" title="Literature">
+          Allows you to manage literature entires: You can update, add, or delete them. This
+          also includes publishing or unpublishing them, depending on your access level.
+        </NavCard>
+        <NavCard to="/" disabled={true} title="Logs">
+          Allows you to manage the server logs: You can download or delete them.
+        </NavCard>
+        <NavCard to="/" disabled={true} title="Users">
+          Allows you to manage users: You can add, update, or delete them.
+        </NavCard>
+      </div>
+    </>
   );
 }
