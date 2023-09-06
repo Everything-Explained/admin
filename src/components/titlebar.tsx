c042a9da4dc976903aa36772b052ec5f0c96af74
Jaeiya <exanix@gmail.com>
2023-09-05 17:15:10 -0700

    feat: add `Titlebar` component

---
+++ src/components/titlebar.tsx
@@ -0,0 +1,27 @@
+import './titlebar.css';
+import { Logo } from './logo';
+import { Link } from './link';
+import { Avatar } from './avatar';
+import { ProfileButton } from './profile-button';
+
+export function Titlebar() {
+  return (
+    <>
+      <div class="titlebar__container flex gap-2 border-neutral-800">
+        <Link to="/">
+          <header class="flex__vcenter gap-3 px-2 py-2 text-2xl">
+            <Logo />
+            <span class="font-semibold text-red-500">
+              EvEx<span class="font-normal text-yellow-400">-Admin</span>
+            </span>
+          </header>
+        </Link>
+        <div class="flex__vcenter flex-1 justify-end rounded-e-lg rounded-s-lg">
+          <span class="mr-2">
+            <ProfileButton />
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
