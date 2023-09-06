300c1002ca42bdeb2addd18725e04c92cb6d5c43
Jaeiya <exanix@gmail.com>
2023-09-05 17:26:22 -0700

    feat(404): finish with new content and style

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -1,3 +1,9 @@
 export function PageNotFound() {
-  return <div>Page Not Found : 404</div>;
+  return (
+    <div class="mt-[5vh] text-center text-3xl text-yellow-400">
+      Page Not Found
+      <br />
+      <div class="mt-10 font-mono text-3xl text-rose-400">┻━┻︵ \\(°□°)/ ︵ ┻━┻</div>
+    </div>
+  );
 }
