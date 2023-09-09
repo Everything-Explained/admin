7823ad793256f8c902cd375038b8ed439235e6b1
Jaeiya <exanix@gmail.com>
2023-09-09 15:44:42 -0700

    feat: add Select component

---
+++ src/components/select.tsx
@@ -0,0 +1,72 @@
+import { For, createSignal, onCleanup, onMount } from 'solid-js';
+import './select.css';
+
+type SelectProps = {
+  items: string[];
+  /* CSS width value */
+  maxHeight: string;
+  /** Default Item */
+  children: string;
+
+  onChange: (val: number) => void;
+};
+
+export function Select({ items, children, onChange, maxHeight }: SelectProps) {
+  let elSelect: HTMLDivElement | undefined;
+  const [open, setOpenState] = createSignal(false);
+  const [title, setTitle] = createSignal(children);
+
+  const adjustedItems = ['None', ...items];
+
+  document.addEventListener('mousedown', closeSelection);
+
+  onMount(() => {
+    if (elSelect) {
+      document.documentElement.style.setProperty('--select-max-height', maxHeight);
+    }
+  });
+
+  function setSelection(index: number) {
+    onChange(index);
+    if (index == -1) return setTitle(children);
+    setTitle(adjustedItems[index + 1]);
+  }
+
+  function closeSelection() {
+    setOpenState(false);
+  }
+
+  function toggleOpen(e: MouseEvent) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    setOpenState(!open());
+  }
+
+  onCleanup(() => {
+    closeSelection();
+    document.removeEventListener('mousedown', closeSelection);
+  });
+
+  return (
+    <>
+      <div
+        ref={elSelect}
+        class="__select flex cursor-pointer gap-4 border-zinc-500 px-3"
+        classList={{ '--open': open() }}
+        onmousedown={toggleOpen}
+      >
+        <label class="cursor-pointer font-normal text-zinc-500">{title()}</label>
+        <div class="select__arrow bg-zinc-500"></div>
+        <div class="select__items bg-gray-700/95">
+          <For each={adjustedItems}>
+            {(item, index) => (
+              <div onMouseDown={() => setSelection(index() - 1)} class="px-3 py-1 text-lg">
+                {item}
+              </div>
+            )}
+          </For>
+        </div>
+      </div>
+    </>
+  );
+}
