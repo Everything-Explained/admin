cc3471faa0036ebac6236eb4245424d5746a0123
Jaeiya <exanix@gmail.com>
2023-09-14 16:44:39 -0700

    upd(index): remove literature route

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import { Home } from './views/home/home';
 import { Route, Router, Routes } from '@solidjs/router';
 import { GlobalProgress } from './components/ui/global-progress';
-import { Literature } from './views/literature';
 import { Login } from './views/login/login';
 import { LoginRouteGuard } from './components/login-route-guard';
 import { PageNotFound } from './views/404';
@@ -34,7 +33,6 @@ render(
           <Route path="/login" component={Login} />
           <Route path="/" component={LoginRouteGuard}>
             <Route path="/" component={Home} />
-            <Route path="/literature" component={Literature} />
             <Route path="/users" component={Users} />
           </Route>
           <Route path="/profile" component={Profile} />
