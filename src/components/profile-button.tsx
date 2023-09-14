36f0429cae79fdc8913adf47cc654e61b0d3c718
Jaeiya <exanix@gmail.com>
2023-09-14 15:33:38 -0700

    feat(profile-button): add global `accessLevel` Accessor
    We need to be able to update the profile icon when a user
    logs in or any time their access level changes.

--- src/components/profile-button.tsx
+++ src/components/profile-button.tsx
@@ -1,14 +1,19 @@
+import { createSignal } from 'solid-js';
 import { Avatar } from './avatar';
 import { Link } from './link';
+import { UserAccessLevel } from '../database/db-user';
+
+export const profileAccessLevel = createSignal<UserAccessLevel>(0);
 
 export function ProfileButton() {
   // TODO: Use auth utility to validate logged-in status
 
   const isAuthed = false;
+  const [accessLevel] = profileAccessLevel;
 
   return (
     <Link to={isAuthed ? '/profile' : '/login'}>
-      <Avatar authed={isAuthed} />
+      <Avatar accessLevel={accessLevel()} />
     </Link>
   );
 }
