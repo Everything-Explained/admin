b1afc78e55d9fd1f6334d713676da2266e93071f
Jaeiya <exanix@gmail.com>
2023-09-14 16:57:37 -0700

    css(404): make table flipper bigger

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -3,7 +3,7 @@ export function PageNotFound() {
     <div class="mt-[5vh] text-center text-3xl text-yellow-400">
       Page Not Found
       <br />
-      <div class="mt-10 font-mono text-3xl text-rose-400">┻━┻︵ \\(°□°)/ ︵ ┻━┻</div>
+      <div class="mt-10 font-mono text-5xl text-rose-400">┻━┻︵ \\(°□°)/ ︵ ┻━┻</div>
     </div>
   );
 }
