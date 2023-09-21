33ea03fb300415e290ce744b3623f8acdc73dbd6
Jaeiya <exanix@gmail.com>
2023-09-21 15:36:54 -0700

    feat(home): add `video` panel and update verbiage

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -4,20 +4,23 @@ import './home.css';
 import { NavCard } from '../../components/nav-card';
 
 export function Home() {
-  const nav = useNavigate();
-  const [startProgress, stopProgress] = useGlobalProgress();
+  // const [startProgress, stopProgress] = useGlobalProgress();
 
   // useBeforeLeave(() => {
-  //     startProgress();
+  //   startProgress();
   // });
 
   return (
     <>
       <header class="my-7 text-center text-4xl">Panel Selection</header>
       <div class="home__container mx-10 mt-4 gap-6">
-        <NavCard to="/login" title="Literature">
-          Allows you to manage literature entires: You can update, add, or delete them. This
-          also includes publishing or unpublishing them, depending on your access level.
+        <NavCard to="/literature" title="Literature">
+          Allows you to manage literature entires: You can update, add, delete, publish, or
+          unpublish them, depending on your access level.
+        </NavCard>
+        <NavCard to="/videos" title="Videos">
+          Allows you to manage video entires: you can update, add, delete, publish, or unpublish
+          them, depending on your access level.
         </NavCard>
         <NavCard to="/" disabled={true} title="Logs">
           Allows you to manage the server logs: You can download or delete them.
