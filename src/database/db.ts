c60252bb52d9f055a03125e21ac14e1304b887d4
Jaeiya <exanix@gmail.com>
2023-09-28 15:20:16 -0700

    upd(db): allow for filtering entries on load
    blah

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,7 +6,7 @@ export interface DBEntry<T> {
   entries: Accessor<T[]>;
   findByID(id: string): T | null;
   fuzzyFind(entryProp: string): T[] | null;
-  loadEntries(): DBResponse<true>;
+  loadEntries(type?: string): DBResponse<true>;
   delete<K>(id: string): DBResponse<K>;
   add<K>(id: string): DBResponse<K>;
   edit<K>(entry: T): DBResponse<K>;
