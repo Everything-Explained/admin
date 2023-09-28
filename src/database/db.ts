10ce2425b16af0a5d2b44222db16354d210e2fac
Jaeiya <exanix@gmail.com>
2023-09-28 15:50:58 -0700

    feat(db): add `context` arg to all applicable methods
    This allows us to specify a custom context under which to
    execute a method; our use-case is categories, under which
    a lot of our content will be placed.

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,10 +4,10 @@ export type DBResponse<T> = Promise<[error: string, null] | [null, T]>;
 
 export interface DBEntry<T> {
   entries: Accessor<T[]>;
-  findByID(id: string): T | null;
-  fuzzyFind(entryProp: string): T[] | null;
+  findByID(id: string, context?: string): T | null;
+  fuzzyFind(entryProp: string, context?: string): T[] | null;
   loadEntries(type?: string): DBResponse<true>;
-  delete<K>(id: string): DBResponse<K>;
-  add<K>(id: string): DBResponse<K>;
-  edit<K>(entry: T): DBResponse<K>;
+  delete<K>(id: string, context?: string): DBResponse<K>;
+  add<K>(id: string, context?: string): DBResponse<K>;
+  edit<K>(entry: T, context?: string): DBResponse<K>;
 }
