87d5c5c7574714c43db0933420744bde126dfcb6
Jaeiya <exanix@gmail.com>
2023-10-15 16:18:26 -0700

    feat(select-field): track selected-index and default-index states
    CSS has also been reduced where applicable and turned into
    tailwind.

--- src/components/input/select-field.tsx
+++ src/components/input/select-field.tsx
@@ -3,21 +3,23 @@ import './select-field.css';
 
 type SelectProps = {
   items: string[];
-  /** CSS height value */
-  maxHeight: string;
-  /** Default Item */
+  /** Defaults to -1 (None) */
+  selectedIndex?: number;
+  /** Select Title */
   children: string;
   width?: string;
   onChange: (val: number) => void;
 };
 
 export function SelectField(props: SelectProps) {
-  const { items, children, width, onChange, maxHeight } = props;
+  const { items, children, width, onChange, selectedIndex } = props;
   let elSelect: HTMLDivElement | undefined;
+  const maxHeight = `${items.length * 3}rem`;
   const [open, setOpenState] = createSignal(false);
   const [title, setTitle] = createSignal(children);
+  const [selectedItem, setSelectedIndex] = createSignal(selectedIndex ?? -1);
   const isDefault = createMemo(() => {
-    return title() == children;
+    return title() == children || selectedItem() == selectedIndex;
   });
   const selectWidth = width ?? '200px';
 
@@ -26,6 +28,7 @@ export function SelectField(props: SelectProps) {
   document.addEventListener('mousedown', closeSelection);
 
   onMount(() => {
+    setSelection(selectedIndex ?? -1);
     if (elSelect) {
       document.documentElement.style.setProperty('--select-max-height', maxHeight);
       document.documentElement.style.setProperty('--select-width', selectWidth);
@@ -34,6 +37,7 @@ export function SelectField(props: SelectProps) {
 
   function setSelection(index: number) {
     onChange(index);
+    setSelectedIndex(index);
     if (index == -1) return setTitle(children);
     setTitle(adjustedItems[index + 1]);
   }
@@ -57,7 +61,7 @@ export function SelectField(props: SelectProps) {
     <>
       <div
         ref={elSelect}
-        class="__select flex cursor-pointer gap-4 px-1"
+        class="__select relative flex cursor-pointer select-none items-center gap-4 px-1 capitalize"
         classList={{
           '--open': open(),
           'border-zinc-500': isDefault(),
@@ -72,9 +76,9 @@ export function SelectField(props: SelectProps) {
           {title()}
         </label>
         <div class="flex-1 text-right">
-          <div class="select__arrow bg-zinc-500"></div>
+          <div class="select__arrow inline-block bg-zinc-500 text-right"></div>
         </div>
-        <div class="select__items bg-gray-700/95">
+        <div class="select__items absolute left-0 top-full max-h-0 w-full overflow-hidden rounded-b-lg bg-gray-700/95 capitalize">
           <For each={adjustedItems}>
             {(item, index) => (
               <div onMouseDown={() => setSelection(index() - 1)} class="px-3 py-1 text-lg">
